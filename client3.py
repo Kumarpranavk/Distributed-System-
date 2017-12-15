11151451f8405dde08f3eea9ebc00a5c1a748d1a
Kumarpranavk <33405219+Kumarpranavk@users.noreply.github.com>
2017-12-15 18:14:49 +0000

    Adding with cashing on client codes
    multiple errors detected and rectified

--- client3.py
+++ client3.py
@@ -5,182 +5,19 @@ import utils
 
 
 class DFSError(IOError):
-
-
-    pass
-
-
-class File(SpooledTemporaryFile):
-
-
-    def __init__(self, filepath, mode='rtc'):
-
-        self.mode = mode
-        self.filepath = filepath
-        host, port = utils.get_host_port(_config['nameserver'])
-        self.srv = utils.get_server(filepath, host, port)
-
-        if self.srv is None:
-            raise DFSError('Impossible to find a server that serve %s.'
-                           % filepath)
-
-        self.last_modified = None
-        SpooledTemporaryFile.__init__(self, _config['max_size'], mode.replace('c', ''))
-
-        host, port = utils.get_host_port(_config['lockserver'])
-        if utils.is_locked(filepath, host, port):
-            raise DFSError('The file %s is locked.' % filepath)
-
-        if 'w' not in mode:
-            host, port = utils.get_host_port(self.srv)
-            with closing(http.client(host, port)) as con:
-                con.request('GET', filepath)
-                response = con.getresponse()
-                self.last_modified = response.getheader('Last-Modified')
-                status = response.status
-
-                if status not in (200, 204):
-                    raise DFSError('Error (%d) while opening file.' % status)
-
-                if status != 204:
-                    self.write(response.read())
-
-                if 'r' in mode:
-                    self.seek(0)
-
-                self.lock_id = None
-
-        if 'a' in mode or 'w' in mode:
-            # automatically gets a lock if we're in write/append mode
-            host, port = utils.get_host_port(_config['lockserver'])
-            self.lock_id = int(utils.get_lock(filepath, host, port))
-
-        if 'c' in mode:
-            File._cache[filepath] = self
-
-    def __exit__(self, exc, value, tb):
-
-
-        self.close()
-
-        if 'c' not in self.mode:
-            return SpooledTemporaryFile.__exit__(self, exc, value, tb)
-
-        return False
-
-    def close(self):
-
-
-        self.flush()
-
-        if 'c' not in self.mode:
-            SpooledTemporaryFile.close(self)
-
-    def flush(self):
-
-
-        SpooledTemporaryFile.flush(self)
-        self.commit()
-
-    def commit(self):
-
-
-        if 'a' in self.mode or 'w' in self.mode:
-            # send the file from the begining
-            self.seek(0)
-            data = self.read()
-            host, port = utils.get_host_port(self.srv)
-            with closing(http.client(host, port)) as con:
-                con.request('PUT', self.filepath + '?lock_id=%s' % self.lock_id,
-                            data)
-
-                response = con.getresponse()
-                # self.last_modified = response.getheader('Last-Modified')
-                status = response.status
-                if status != 200:
-                    raise DFSError('Error (%d) while committing change to'
-                                   ' the file.' % status)
-
-        if self.lock_id is not None:
-            host, port = utils.get_host_port(_config['lockserver'])
-            utils.revoke_lock(self.filepath, host, port, self.lock_id)
-
-    @staticmethod
-    def from_cache(filepath):
-
-        if filepath in File._cache:
-            f = File._cache[filepath]
-
-            host, port = utils.get_host_port(_config['nameserver'])
-            fs = utils.get_server(filepath, host, port)
-            host, port = utils.get_host_port(fs)
-
-            with closing(http.client(host, port)) as con:
-                con.request('HEAD', filepath)
-
-                if (f.last_modified ==
-                        con.getresponse().getheader('Last-Modified')):
-                    f.seek(0)
-                    return f
-                else:
-                    del File._cache[filepath]
-
-        return None
-
-
-def unlink(filepath, lock_id=None):
-
-
-    host, port = utils.get_host_port(_config['nameserver'])
-    fs = utils.get_server(filepath, host, port)
-    host, port = utils.get_host_port(fs)
-
-    with closing(http.client(host, port)) as con:
-        con.request('DELETE', filepath + '?lock_id=%s' % lock_id)
-
-        status = con.getresponse().status
-
-        if status != 200:
-            raise DFSError('Error (%d) while deleting %s.' %
-                           (status, filepath))
-
-
-def rename(filepath, newfilepath):
-
-
-    with open(filepath) as f:
-        with open(newfilepath, 'w') as nf:
-            nf.write(f.read())
-
-        unlink(filepath, f.lock_id)
-
-
-open = File
-
-_config = {
-    'nameserver': None,
-    'lockserver': None,
-    'max_size': 1024 ** 2,
-}  # default
-File._cache = {}
-utils.load_config(_config, 'client.dfs.json')
-from contextlib import closing
-import http.client
-from tempfile import SpooledTemporaryFile
-import utils
-
-
-class DFSError(IOError):
-
+    """To send error in case of file locked"""
 
     pass
 
 
 class File(SpooledTemporaryFile):
-
+    """Is a distant file, it's stored in memory if it size if less than
+       the max_size parameter, otherwise it's stored on the disk.
+    """
 
     def __init__(self, filepath, mode='rtc'):
-
+        """filepath: the path of the distant file
+        """
         self.mode = mode
         self.filepath = filepath
         host, port = utils.get_host_port(_config['nameserver'])
@@ -235,7 +72,7 @@ class File(SpooledTemporaryFile):
         return False
 
     def close(self):
-
+        """Send the change to the DFS, and close the file."""
 
         self.flush()
 
@@ -243,13 +80,13 @@ class File(SpooledTemporaryFile):
             SpooledTemporaryFile.close(self)
 
     def flush(self):
-
+        """Flush the data to the server."""
 
         SpooledTemporaryFile.flush(self)
         self.commit()
 
     def commit(self):
-
+        """Send the local file to the remote fileserver."""
 
         if 'a' in self.mode or 'w' in self.mode:
             # send the file from the begining
@@ -273,7 +110,7 @@ class File(SpooledTemporaryFile):
 
     @staticmethod
     def from_cache(filepath):
-
+        """save file in local disk"""
         if filepath in File._cache:
             f = File._cache[filepath]
 
@@ -295,7 +132,8 @@ class File(SpooledTemporaryFile):
 
 
 def unlink(filepath, lock_id=None):
-
+    """Delete the file from the filesystem.
+       If lock_id is provided, it's used to delete the file."""
 
     host, port = utils.get_host_port(_config['nameserver'])
     fs = utils.get_server(filepath, host, port)
@@ -312,7 +150,7 @@ def unlink(filepath, lock_id=None):
 
 
 def rename(filepath, newfilepath):
-
+    """Rename filepath to newfilepath."""
 
     with open(filepath) as f:
         with open(newfilepath, 'w') as nf:
